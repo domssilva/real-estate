91526a472ff7a7a34ba9d697e0da5f59c1e3287c
domssilva <silva@programmer.net>
2020-09-05 00:16:46 +0200

    setup routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 
+import Rent from './Pages/Rent';
+
 function App() {
   return (
-    <div className="home">
-      real estate app
-    </div>
+    <Switch>
+      <Route exact path='/rent' component={Rent}/>
+    </Switch>
   );
 }
 
